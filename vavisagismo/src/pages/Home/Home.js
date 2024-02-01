cb1983354307a2d6cc864b5206218ce4b2ab8d3c
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-02-01 11:57:21 -0300

    Acerta a responsividade

--- vavisagismo/src/pages/Home/Home.js
+++ vavisagismo/src/pages/Home/Home.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import styles from "./Home.module.css"
+import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.home}>
+      <div>
         <h1>Seja Bem Vindo!</h1>
+      </div>
+      <div>
         <p>Faça seu cadastro ou login para acessar a sua página!</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
