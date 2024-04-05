3152e59c85cc96a7fe01f998c719064b3dfc0da4
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-04-05 00:43:29 -0300

    .

--- vavisagismo/src/pages/System/Home/HomeSystem.js
+++ vavisagismo/src/pages/System/Home/HomeSystem.js
@@ -11,15 +11,8 @@ const HomeSystem = () => {
   useEffect(() => {
     if (cancelled) return;
 
-    
-
     if (user) {
       setUserName(user.displayName);
-      if(user.photoURL) {
-        setUserPhoto(user.photoURL);
-      } else {
-        setUserPhoto("https://firebasestorage.googleapis.com/v0/b/vmavisagismo.appspot.com/o/logobremoved.png?alt=media&token=e259ce3f-06bd-4e9a-9429-3e1a85a3eb99")
-      }
  
     }
 
